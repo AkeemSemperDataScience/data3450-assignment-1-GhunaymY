8fcb2314b1f9982d20f40ecf6f2a976fb36c43fd
Ghunaym Yahya <ghunaym@Ghunayms-MacBook-Pro.local>
2025-09-26 17:22:26 -0600

    Age_Splitter function finished

--- asn1_function_sheet.py
+++ asn1_function_sheet.py
@@ -17,7 +17,11 @@ def age_splitter(df, col_name, age_threshold):
         - df_below: DataFrame with rows where age is below the threshold.
         - df_above_equal: DataFrame with rows where age is above or equal to the threshold.
     """
-    pass
+    # Using .copy() to ensure a seperate data frame is created.
+    
+    df_below = df[df[col_name] < age_threshold].copy() # Select the Rows in Col_name that are lower than the threshold
+    df_above_equal = df[df[col_name] >= age_threshold].copy() # Select the Rows in Col_name that are greather than or equal to the threshold
+    return df_below, df_above_equal # Return both of the dataframes as a tuple
     
 def effectSizer(df, num_col, cat_col):
     """
